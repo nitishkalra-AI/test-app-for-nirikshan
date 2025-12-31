38ce7c67e874e2388839d07219b1a739953bc3a1
Nitish Kalra <252281102+nitishkalra-AI@users.noreply.github.com>
2025-12-31 11:39:56 +0530

    test pr with issues

--- src/App.js
+++ src/App.js
@@ -19,6 +19,22 @@ function App() {
         </a>
       </header>
     </div>
+    <a
+    className="App-link"
+    href="https://reactjs.org"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    Learn React
+  </a>
+  <a
+  className="App-link"
+  href="https://reactjs.org"
+  target="_blank"
+  rel="noopener noreferrer"
+>
+  Learn React
+</a>
   );
 }
 
