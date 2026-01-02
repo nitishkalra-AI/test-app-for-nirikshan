23b3490883f1c5391761a2abec734396c7ce00ad
Nitish Kalra <252281102+nitishkalra-AI@users.noreply.github.com>
2026-01-02 19:13:14 +0530

    revert

--- src/SamplePage.js
+++ src/SamplePage.js
@@ -26,7 +26,14 @@ function SamplePage() {
         >
           Learn React
         </a>
-        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
         <a
           className="App-link"
           href="https://reactjs.org"
