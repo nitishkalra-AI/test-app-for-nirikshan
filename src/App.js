88c6e0b7442fbdf339894995031f6b3099bbd86f
Nitish Kalra <252281102+nitishkalra-AI@users.noreply.github.com>
2026-01-26 17:06:50 +0530

    remove error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Address from './Address';
-import ErrorComponent from './ErrorComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Address />
-      <ErrorComponent />
     </div>
   );
 }
