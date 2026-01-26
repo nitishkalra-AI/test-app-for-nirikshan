2f77b9fc346c77b4bea302b05f4be917dc9fa912
Nitish Kalra <252281102+nitishkalra-AI@users.noreply.github.com>
2026-01-26 17:09:26 +0530

    test branch 5
    test branch 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Address from './Address';
+import BuggyComponent from './BuggyComponent';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Address />
+      <BuggyComponent />
     </div>
   );
 }
